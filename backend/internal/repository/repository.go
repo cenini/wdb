300e41c10fff9e1f4331752341d26d8e732866be
Niklas Nilsson <nini@netlight.com>
2023-10-06 17:10:49 +0200

    Create function skeletons in repository

--- backend/internal/repository/repository.go
+++ backend/internal/repository/repository.go
@@ -13,3 +13,35 @@ func NewRepository(db *bun.DB) *Repository {
 		db: db,
 	}
 }
+
+func (r *Repository) CreateUser() {
+
+}
+
+func (r *Repository) DeleteUser() {
+
+}
+
+func (r *Repository) CreateItem() {
+
+}
+
+func (r *Repository) UpdateItem() {
+
+}
+
+func (r *Repository) DeleteItem() {
+
+}
+
+func (r *Repository) CreatePhoto() {
+
+}
+
+func (r *Repository) UpdatePhoto() {
+
+}
+
+func (r *Repository) DeletePhoto() {
+
+}
