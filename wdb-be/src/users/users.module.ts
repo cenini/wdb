b6cf26c9c0488fe0d6a6d374b5fd3392e6b4e6ff
Niklas Nilsson <nini@netlight.com>
2023-11-08 18:33:32 +0100

    Add users module

--- wdb-be/src/users/users.module.ts
+++ wdb-be/src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   providers: [UsersService],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
