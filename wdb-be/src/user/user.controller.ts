62c8946575da219ad3ec44bb8a06d0202ca35413
Niklas Nilsson <nini@netlight.com>
2023-11-14 19:58:42 +0100

    Improve error handling during user creation

--- wdb-be/src/user/user.controller.ts
+++ wdb-be/src/user/user.controller.ts
@@ -1,20 +1,17 @@
 import {
   Body,
   Controller,
-  ExecutionContext,
-  Get,
-  HttpCode,
+  HttpException,
   HttpStatus,
-  InternalServerErrorException,
   Post,
   Request,
-  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User as UserModel } from '@prisma/client';
 import { Public } from '../auth/auth.guard';
 import * as bcrypt from 'bcryptjs';
 import { v4 as uuidv4 } from 'uuid';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Controller('v1/users')
 export class UserController {
@@ -24,14 +21,25 @@ export class UserController {
   @Post()
   async signupUser(
     @Body() userData: { email: string; password: string },
+    @Request() req: Request,
   ): Promise<UserModel> {
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(userData.password, salt);
-    return await this.userService.createUser({
-      email: userData.email,
-      handle: uuidv4(),
-      salt: salt as string,
-      passhash: hash as string,
-    });
+    try {
+      return await this.userService.createUser({
+        email: userData.email,
+        handle: uuidv4(),
+        salt: salt as string,
+        passhash: hash as string,
+      });
+    } catch (error) {
+      if (error.code == 'P2002') {
+        throw new HttpException('Conflicting e-mails', HttpStatus.CONFLICT);
+      }
+      if (error instanceof PrismaClientKnownRequestError) {
+        throw new HttpException('Request was bad', HttpStatus.BAD_REQUEST);
+      }
+      throw error;
+    }
   }
 }
