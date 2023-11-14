4e0d2606b9200a165c5d34c1127149eab272cbd5
Niklas Nilsson <nini@netlight.com>
2023-11-14 15:59:55 +0100

    Add some dummy frontend code with expo

--- wdb-be/src/main.ts
+++ wdb-be/src/main.ts
@@ -4,6 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // add specific origins here later
 
   const config = new DocumentBuilder()
     .setTitle('wdb api')
