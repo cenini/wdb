4b677affe5782b87f23d9613cc704a9e019cc691
Niklas Nilsson <nini@netlight.com>
2023-11-15 12:36:55 +0100

    Improve auth flow

--- wdb-fe/App.tsx
+++ wdb-fe/App.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { createContext, useEffect, useMemo, useReducer, useState } from 'react';
 import { StatusBar } from "expo-status-bar";
 import * as ImagePicker from 'expo-image-picker';
 import { StyleSheet, View} from "react-native";
+import * as SecureStore from 'expo-secure-store';
+
 
 import Button from './components/PhotoButton'; 
 import ImageViewer from './components/ImageViewer';
@@ -10,16 +12,99 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignupScreen from './components/SignupScreen';
 import LoginScreen from './components/LoginScreen';
+import axios, { HttpStatusCode } from 'axios';
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 const Stack = createNativeStackNavigator();
+export const AuthContext = createContext(null);
 
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(null);
 
+  const [state, dispatch] = useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  );
+
+
+  useEffect(() => {
+    // Fetch the token from storage then navigate to our appropriate place
+    const bootstrapAsync = async () => {
+      let userToken: string;
+
+      try {
+        userToken = await SecureStore.getItemAsync('userToken');
+      } catch (e) {
+        // Restoring token failed
+        // User has to sign in again
+        return
+      }
+
+      // After restoring token, we may need to validate it in production apps
+      try {
+        const response = await axios.get("http://localhost:3000/v1/auth/profile")
+        if (response.status != HttpStatusCode.Ok) {
+          // log that the user token has expired
+          return
+        }
+      } catch (error) {
+        // log that the token has expired
+        return
+      }
+
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  const authContext = useMemo(
+    () => ({
+      login: async (email: string, password: string) => {
+        // In a production app, we need to send some data (usually username, password) to server and get a token
+        // We will also need to handle errors if sign in failed
+        // After getting token, we need to persist the token using `SecureStore`
+        // In the example, we'll use a dummy token
+        const loginUrl = "http://localhost:3000/v1/auth/login"
+        const response = await axios.post(loginUrl, { "email": email, "password": password})
+        await SecureStore.setItemAsync('userToken', response.data.token)
+        dispatch({ type: 'SIGN_IN', token: response.data.token });
+      },
+      signOut: () => dispatch({ type: 'SIGN_OUT' }),
+    }),
+    []
+  );
+
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      // allowsEditing: true,
       allowsMultipleSelection: true,
       selectionLimit: 10,
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
@@ -46,13 +131,15 @@ export default function App() {
     //   </View>
     //   <StatusBar style="auto" />
     // </View>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+          <Stack.Screen name="Signup" component={SignupScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
 
--- wdb-fe/components/LoginScreen.tsx
+++ wdb-fe/components/LoginScreen.tsx
@@ -4,61 +4,64 @@ import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Button from './Button';
 import axios, { HttpStatusCode } from 'axios';
+import { useContext } from 'react';
+import { AuthContext } from '../App';
 
 const loginUrl = "http://localhost:3000/v1/auth/login"
 
 export default function LoginScreen() {
-  const navigation = useNavigation()
+  const { login } = useContext(AuthContext);
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [loginText, setLoginText] = React.useState('');
   const [loginSuccessful, setLoginSuccessful] = React.useState(false);
   const [rememberMe, setRememberMe] = React.useState(false);
+  const navigation = useNavigation()
 
-  async function login(email: string, password: string) : Promise<void> {
-    try {
-      const response = await axios.post(loginUrl, { "email": email, "password": password})
-      if (response.status != HttpStatusCode.Ok) {
-        setLoginText('Login failed.')
-        setLoginSuccessful(false)
-        return;
-      }
-    } catch (error) {
-      // if (error.response) {
-      //   // The request was made and the server responded with a status code
-      //   // that falls out of the range of 2xx
-      //   console.log(error.response.data);
-      //   console.log(error.response.status);
-      //   console.log(error.response.headers);
-      //   if (error.response.status == HttpStatusCode.Conflict) {
-      //     setLoginText('A user with that e-mail has already been registered.')
-      //     setLoginSuccessful(false)
-      //   } else if (error.response.status < 500 ) {
-      //     setLoginText('The user was not accepted. You know what you did.')
-      //     setLoginSuccessful(false)
-      //   } else {
-      //     setLoginText('There was a server error. Try again later.')
-      //     setLoginSuccessful(false)
-      //   }
-      // } else if (error.request) {
-      //   // The request was made but no response was received
-      //   // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      //   // http.ClientRequest in node.js
-      //   console.log(error.request);
-      //   setLoginText('The user could not be created at this time. Try again later.')
-      //   setLoginSuccessful(false)
-      // } else {
-      //   // Something happened in setting up the request that triggered an Error
-      //   // Client side issue, should be traced.
-      //   setLoginText('The user could not be created at this time. Try again later.')
-      //   setLoginSuccessful(false)
-      //   console.log('Error', error.message);
-      // }
-      return;
-    }
-    setLoginText('Login successful!')
-    setLoginSuccessful(true)
-  }
+  // async function login(email: string, password: string) : Promise<void> {
+  //   try {
+  //     const response = await axios.post(loginUrl, { "email": email, "password": password})
+  //     if (response.status != HttpStatusCode.Ok) {
+  //       setLoginText('Login failed.')
+  //       setLoginSuccessful(false)
+  //       return;
+  //     }
+  //   } catch (error) {
+  //     // if (error.response) {
+  //     //   // The request was made and the server responded with a status code
+  //     //   // that falls out of the range of 2xx
+  //     //   console.log(error.response.data);
+  //     //   console.log(error.response.status);
+  //     //   console.log(error.response.headers);
+  //     //   if (error.response.status == HttpStatusCode.Conflict) {
+  //     //     setLoginText('A user with that e-mail has already been registered.')
+  //     //     setLoginSuccessful(false)
+  //     //   } else if (error.response.status < 500 ) {
+  //     //     setLoginText('The user was not accepted. You know what you did.')
+  //     //     setLoginSuccessful(false)
+  //     //   } else {
+  //     //     setLoginText('There was a server error. Try again later.')
+  //     //     setLoginSuccessful(false)
+  //     //   }
+  //     // } else if (error.request) {
+  //     //   // The request was made but no response was received
+  //     //   // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+  //     //   // http.ClientRequest in node.js
+  //     //   console.log(error.request);
+  //     //   setLoginText('The user could not be created at this time. Try again later.')
+  //     //   setLoginSuccessful(false)
+  //     // } else {
+  //     //   // Something happened in setting up the request that triggered an Error
+  //     //   // Client side issue, should be traced.
+  //     //   setLoginText('The user could not be created at this time. Try again later.')
+  //     //   setLoginSuccessful(false)
+  //     //   console.log('Error', error.message);
+  //     // }
+  //     return;
+  //   }
+  //   setLoginText('Login successful!')
+  //   setLoginSuccessful(true)
+  // }
 
   return (
     <>
