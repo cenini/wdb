d4bb41019aedbee99c808f03a992c3e040d1e639
Niklas Nilsson <nini@netlight.com>
2023-12-05 17:42:49 +0100

    Get started on home screen

--- wdb-fe/components/HomeScreen.tsx
+++ wdb-fe/components/HomeScreen.tsx
@@ -7,7 +7,7 @@ import {
 } from "react";
 import { StatusBar } from "expo-status-bar";
 import * as ImagePicker from "expo-image-picker";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import PhotoButton from "./PhotoButton";
 import Button from "./Button";
 import { AuthContext } from "../App";
@@ -17,8 +17,6 @@ import { NameTagModel } from "../models/TagModel";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NewItemsContext } from "./AppScreen";
-
-const PlaceholderImage = require("../assets/images/background-image.png");
 
 export default function HomeScreen() {
   const { addImages } = useContext(NewItemsContext);
@@ -41,7 +39,22 @@ export default function HomeScreen() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
+      {/* <View style={styles.imageContainer}>
+      </View> */}
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>
+          Make your wardrobe 
+          {'\n'}
+          make sense
+        </Text> 
+      </View>
+      <View style={styles.mainContainer}>
+        <View style={styles.heroImageContainer}>
+          <Image source={require("../assets/images/hero.webp")} style={styles.heroImage} /> 
+        </View>
+        <Text style={styles.headerText}>
+          Some main text here
+        </Text> 
       </View>
       <View style={styles.footerContainer}>
         <PhotoButton
@@ -60,6 +73,45 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
+    justifyContent: 'flex-start', // Start aligning items from the top
+  },
+  headerContainer: {
+    flex: 1, // Adjust this flex value to control the size of this container
+    alignItems: "center",
+    justifyContent: 'center', // Center content horizontally and vertically
+    width: '100%', // Full width
+    padding: 20, // Adjust padding as necessary
+  },
+  headerText: {
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center",
+    color: "#fff",
+    // fontFamily: "Your-Font-Family",
+  },
+  mainContainer: {
+    flex: 2, // Adjust this flex value to control the size of this container
+    alignItems: "center",
+    justifyContent: 'center', // Center content horizontally and vertically
+    width: '100%',
+  },
+  heroImageContainer: {
+    width: '90%', // Adjust as necessary
+    height: 300, // Or use aspectRatio for dynamic sizing
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  heroImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  mainText: {
+    fontWeight: "normal",
+    fontSize: 16,
+    textAlign: "center",
+    color: "#fff", // Assuming white text
+    // fontFamily: "Your-Font-Family",
   },
   imageContainer: {
     flex: 1,
